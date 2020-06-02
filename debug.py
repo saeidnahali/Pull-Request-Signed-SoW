4cad334b784831053e3964dd519edca4820ac818
Mohammad Pakizeh Jam <61758931+pakizehjam@users.noreply.github.com>
2020-06-01 22:03:41 -0700

    I debug the cods.

--- debug.py
+++ debug.py
@@ -1,15 +1,19 @@
 a = int(input())
 b = int(input())
 c = int(input())
-def lone_sum(a, b, c):
-    if a >= b:
+def lone(a, b, c):
+    if a == b and b != c:
         return c
-    elif a == c:
+
+    elif a == c and a != b:
         return b
-    elif b == c:
+
+    elif b == c and a != b:
         return a
+
     elif a == b and a == c and b == c:
         return 0
+
     else:
         return a+b+c
 print(lone(a,b,c))    
